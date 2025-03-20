5e11ec961b6a226c65fc741ddfc1838ea11622f7
Lyla Lynn <lyladlynn@gmail.com>
2025-03-20 13:39:23 -0500

    added form to HTML file and put the root in a variable in js

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 /** Steps:
 1. create a state
-2. select the root div and store in a variable
-3. target the form from the HTML and store it in a variable or create it dynamically in js
-4. create a render function
-5. create an async function to fetch the events and put them in a list on the page
-6. create a button for each party in the list
-7. create an eventListener that is attached to each delete button
-8. create an eventListener for the form to add new parties to the list
+2. add the api in a variable
+3. select the root div and store in a variable
+4. target the form from the HTML and store it in a variable or create it dynamically in js
+4a. if creating dynamically, add an eventListener('DOMContentLoaded' before the init function (which will be inside the {} of the eventListener))
+5. create a render function
+6. create an async function to fetch the events and put them in a list on the page
+7. create a button for each party in the list
+8. create an eventListener that is attached to each delete button
+9. create an eventListener for the form to add new parties to the list
  */
+
+// Step 2:
+const baseUrl = 'https://fsa-crud-2aa9294fe819.herokuapp.com/api/COHORT_CODE/events'
+
+// Step 1:
+const state = {
+    events: [],
+}
+
+// Step 3:
+const root = document.querySelector('#root');
